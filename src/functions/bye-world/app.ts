7d463d0d0854a1f53bc6f4e533b0c105db949d31
Nima Golzar Soufiani <nima.soufiani@rippleenergy.com>
2022-11-11 15:33:07 +0000

    implement code layer

--- src/functions/bye-world/app.ts
+++ src/functions/bye-world/app.ts
@@ -1,7 +1,10 @@
 import axios from "axios"
+import { subtraction } from "commands"
 
 export const lambdaHandler = async () => {
-    console.log("in hello handler")
+    console.log("in bye handler")
+    const sub = subtraction(1, 2)
+    console.log(sub)
     const response = await axios.get("https://jsonplaceholder.typicode.com/todos/2")
     console.log(response.data)
 }
--- src/functions/hello-world/app.ts
+++ src/functions/hello-world/app.ts
@@ -1,7 +1,11 @@
 import axios from "axios"
 
+import { addition } from "commands"
+
 export const lambdaHandler = async () => {
     console.log("in hello handler")
+    const add = addition(1, 2)
+    console.log(add)
     const response = await axios.get("https://jsonplaceholder.typicode.com/todos/1")
     console.log(response.data)
 }
