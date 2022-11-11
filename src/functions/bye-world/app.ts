ef8999bc5142290c46c4c1fd36e585bc9b3d5c01
Nima Golzar Soufiani <nima.soufiani@rippleenergy.com>
2022-11-11 14:48:22 +0000

    deps working

--- src/functions/bye-world/app.ts
+++ src/functions/bye-world/app.ts
@@ -1,3 +1,7 @@
+import axios from "axios"
+
 export const lambdaHandler = async () => {
-    console.log('in bye handler');
-};
+    console.log("in hello handler")
+    const response = await axios.get("https://jsonplaceholder.typicode.com/todos/2")
+    console.log(response.data)
+}
--- src/functions/hello-world/app.ts
+++ src/functions/hello-world/app.ts
@@ -1,3 +1,7 @@
+import axios from "axios"
+
 export const lambdaHandler = async () => {
-    console.log('in hello handler');
-};
+    console.log("in hello handler")
+    const response = await axios.get("https://jsonplaceholder.typicode.com/todos/1")
+    console.log(response.data)
+}
