3f66df3805cff9fd7bffe4d4a1e4c6758b10587b
Nima Golzar Soufiani <nima.soufiani@rippleenergy.com>
2022-11-17 16:54:14 +0000

    it works omg

--- src/functions/hello-world/app.ts
+++ src/functions/hello-world/app.ts
@@ -1,11 +1,11 @@
 import axios from "axios"
 
-// import { addition } from "/opt/nodejs/commands"
+import { addition } from "/opt/nodejs"
 
 export const lambdaHandler = async () => {
     console.log("in hello handler")
-    // const add = addition(1, 2)
-    // console.log(add)
+    const add = addition(1, 2)
+    console.log(add)
     const response = await axios.get("https://jsonplaceholder.typicode.com/todos/1")
     console.log(response.data)
 }
